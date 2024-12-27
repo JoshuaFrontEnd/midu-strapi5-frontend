e1693dfa0afeb0cf3c23dc7132c24395be8a91a9
Joshua <hola@joshua.cl>
2024-12-27 12:26:48 -0300

    Agregando y renderizando ProductCategory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Categories } from '@/components/Categories';
 import { Hero } from '@/components/Hero';
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Categories />
+    </>
+  );
 }
---
+++ src/lib/get-categories.ts
@@ -0,0 +1,15 @@
+import { query } from './strapi';
+
+const { STRAPI_HOST } = process.env;
+
+export const getCategories = () => {
+  return query(
+    'product-categories?fields[0]=name&fields[1]=slug&fields[2]=description&populate[image][fields][0]=url'
+  ).then(res => {
+    return res.data.map(category => {
+      const { name, slug, description, image: rawImage } = category;
+      const image = `${STRAPI_HOST}${rawImage.url}`;
+      return { name, slug, description, image };
+    });
+  });
+};
